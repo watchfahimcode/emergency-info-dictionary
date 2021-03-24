94363962121c29ba67cc4a635366b7cfddce0c17
Moshiur Tonmoy <18201027@uap-bd.edu>
2021-03-25 03:05:16 +0600

    Bazar & Fire Station Info Added in Database

--- Emergency_InfoDirectory/Emergency_InfoDirectory/settings.py
+++ Emergency_InfoDirectory/Emergency_InfoDirectory/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bazar.apps.BazarConfig',
+    'fire_station.apps.FireStationConfig',
 ]
 
 MIDDLEWARE = [
