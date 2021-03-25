ed012ff7fd8f582c9736dff925572ff47a3cf761
Md Fahim Rahman <fahimrrahman17@gmail.com>
2021-03-25 14:24:14 +0600

    Added Navigation Bar Registratin Form and Login Form

--- Emergency_InfoDirectory/Emergency_InfoDirectory/settings.py
+++ Emergency_InfoDirectory/Emergency_InfoDirectory/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    "/user/static",
+
+]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
