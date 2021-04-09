7413ad49c353371fa6072baf3aa1e315c01876c6
Md Fahim Rahman <fahimrrahman17@gmail.com>
2021-04-09 22:20:45 +0600

    Added Exception of Exisiting User

--- Emergency_InfoDirectory/user/signals.py
+++ Emergency_InfoDirectory/user/signals.py
@@ -2,13 +2,22 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from .models import Profile
 from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
 @receiver(post_save,sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance,about='')
 
+    else:
+        try:
+            instance.profile.save()
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
+
+
 
 @receiver(post_save, sender=User)
 def update_profile(sender, instance, **kwargs):
     instance.profile.save()
+
